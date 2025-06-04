edbbc13ee88e87d3fa5b005b7542441f3936a1c0
taha khan <Taha@TAF1158.com>
2025-06-04 17:11:43 +0500

    updated packages for flutter 3.32.1

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/waleed/fvm/versions/3.29.3"
-export "FLUTTER_APPLICATION_PATH=/Users/waleed/Documents/Flutter Project/remember_my_love_app"
+export "FLUTTER_ROOT=C:\Users\Taha\fluttersdk"
+export "FLUTTER_APPLICATION_PATH=D:\projects\remember_my_love_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.5"
-export "FLUTTER_BUILD_NUMBER=18"
+export "FLUTTER_BUILD_NAME=1.0.6"
+export "FLUTTER_BUILD_NUMBER=19"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
